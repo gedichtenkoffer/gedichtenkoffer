92fa8eb807660eda408bad7de8fa2bd7bc5bdd10
nick <noobping@users.noreply.github.com>
2023-05-19 00:10:49 +0200

    open and close menu

--- assets/resize.js
+++ assets/resize.js
@@ -1,18 +1,17 @@
 window.addEventListener('resize', function () {
     const menu = document.getElementById('menu');
-    const btn = document.getElementById('toggle');
     if (window.innerWidth >= 800) {
         menu.style.display = 'block';
-        btn.style.display = 'none';
     } else if (window.innerWidth < 800 && menu.style.display !== 'block') {
         menu.style.display = 'none';
-        btn.style.display = 'block';
     }
 });
 
 document.getElementById('toggle').addEventListener('click', function () {
-    const btn = document.getElementById('toggle');
-    const menu = document.getElementById('menu');
-    menu.style.display = 'block';
-    btn.style.display = 'none';
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
 });
