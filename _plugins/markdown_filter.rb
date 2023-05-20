3c4bf7a69b1cd02d6b8f33256b2d7ceb4f003fa6
nick <noobping@users.noreply.github.com>
2023-05-20 18:02:23 +0200

    use the project root

--- _plugins/markdown_filter.rb
+++ _plugins/markdown_filter.rb
@@ -1,10 +1,13 @@
 module Jekyll
   module MarkdownListFilter
-    def markdown_list(input)
+    def markdown_list(input, site)
       md_files = []
-      Dir.glob(File.join(input, '**', '*.md')) do |file|
-        # Replace .md extension with .html and push to the array
-        md_files << file.sub(input, '').sub('.md', '.html')
+      source = site.config['source']
+      destination = site.config['destination']
+      Dir.glob(File.join(source, '**', '*.md')) do |file|
+        html_file = file.sub(source, destination).sub('.md', '.html')
+        # Append the relative path to the array
+        md_files << html_file.sub(destination, '')
       end
       md_files
     end
