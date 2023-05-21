d86f4302b4298f2795b3e210e9d190efbb46619b
nick <noobping@users.noreply.github.com>
2023-05-21 11:14:03 +0200

    typo

--- _plugins/read_filter.rb
+++ _plugins/read_filter.rb
@@ -1,18 +1,18 @@
 module Jekyll
-    module readFilter
-        def read(input)
-            return "" if input.nil?
+  module ReadFilter
+    def read(input)
+      return "" if input.nil?
 
-            if File.exist?(input)
-                File.read(input)
-            else
-                raise "File #{input} not found."
-            end
-        rescue StandardError => e
-            Jekyll.logger.error "Error reading file: #{e.message}"
-            input
-        end
+      if File.exist?(input)
+        File.read(input)
+      else
+        raise "File #{input} not found."
+      end
+    rescue StandardError => e
+      Jekyll.logger.error "Error reading file: #{e.message}"
+      input
     end
+  end
 end
 
-Liquid::Template.register_filter(Jekyll::readFilter)
+Liquid::Template.register_filter(Jekyll::ReadFilter)
