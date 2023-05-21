f9f8bfb753b2f4123fb8306297c57b84d1f10a3e
nick <noobping@users.noreply.github.com>
2023-05-21 22:49:32 +0200

    rerister btn

--- register-sw.js
+++ register-sw.js
@@ -1,28 +1,68 @@
 ---
 ---
 
-document.addEventListener('DOMContentLoaded', function () {
-    if ('serviceWorker' in navigator) {
-        const swIntegrity = {{ '/sw.js' | source_path | read | process_content | minify: 'js' | sri_hash | jsonify }};
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.getElementsByTagName('button');
 
-        fetch('/sw.js')
-        .then(response => response.arrayBuffer())
-        .then(buffer => crypto.subtle.digest('SHA-384', buffer))
-        .then(hash => {
-            let hashArray = Array.from(new Uint8Array(hash)); 
-            let hexString = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-            let base64String = btoa(hexString.match(/\w{2}/g).map(a => String.fromCharCode(parseInt(a, 16))).join(''));
-            return base64String;
-        })
-        .then(base64Hash => {
-            if (`sha384-${base64Hash}` === swIntegrity) {
-                return navigator.serviceWorker.register('/sw.js');
-            } else {
-                console.log(`Expected ${swIntegrity}, but got sha384-${base64Hash}`);
-                return Promise.reject(new Error('Service Worker integrity check failed'));
-            }
-        })
-        .then(reg => console.log('Service worker registered'))
-        .catch(err => console.error(err));
+    const registerServiceWorker = () => {
+        if ('serviceWorker' in navigator) {
+            const swIntegrity = {{ '/sw.js' | source_path | read | process_content | minify: 'js' | sri_hash | jsonify }};
+
+            fetch('/sw.js')
+                .then((response) => {
+                    return response.arrayBuffer();
+                })
+                .then((buffer) => {
+                    return crypto.subtle.digest('SHA-384', buffer);
+                })
+                .then((hash) => {
+                    const hashArray = Array.from(new Uint8Array(hash));
+                    const hexString = hashArray.map((b) => {
+                        return ('0' + b.toString(16)).slice(-2);
+                    }).join('');
+                    const base64String = btoa(hexString.match(/\w{2}/g).map((a) => {
+                        return String.fromCharCode(parseInt(a, 16));
+                    }).join(''));
+                    return base64String;
+                })
+                .then((base64Hash) => {
+                    if ('sha384-' + base64Hash === swIntegrity) {
+                        return navigator.serviceWorker.register('/sw.js');
+                    } else {
+                        console.log('Expected ' + swIntegrity + ', but got sha384-' + base64Hash);
+                        return Promise.reject(new Error('Service Worker integrity check failed'));
+                    }
+                })
+                .then(() => {
+                    console.log('Service worker registered');
+                    navigator.serviceWorker.addEventListener('message', (e) => {
+                        if (e.data && e.data.command === 'progress') {
+                            const progress = document.getElementById('sw-progress');
+                            const parts = e.data.message.split('/');
+                            const loaded = Number(parts[0]);
+                            const total = Number(parts[1]);
+                            progress.value = (loaded / total) * 100;
+                        }
+
+                        // Hide the progress bar when progress is at 0%
+                        if (value === 0) {
+                            progress.style.display = 'none';
+                        } else {
+                            progress.style.display = '';
+                        }
+                    });
+                    // remove event listeners
+                    for (let i = 0; i < buttons.length; i++) {
+                        buttons[i].removeEventListener('click', registerServiceWorker);
+                    }
+                })
+                .catch((err) => {
+                    return console.log(err);
+                });
+        }
+    };
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', registerServiceWorker);
     }
 });
