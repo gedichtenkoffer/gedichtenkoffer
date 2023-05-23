74f5ba7c970ad96a01c565e1a0de39a66b523de2
nick <noobping@users.noreply.github.com>
2023-05-23 19:53:48 +0200

    open and close menu

--- assets/menu.js
+++ assets/menu.js
@@ -1,10 +1,17 @@
 document.querySelectorAll('.toggle-submenu').forEach((a) => {
     a.addEventListener('click', (e) => {
         e.preventDefault();
-        let submenu = e.target.nextElementSibling;
+        const submenu = e.target.nextElementSibling;
         if (submenu) {
             submenu.classList.toggle('show');
             e.target.classList.toggle('active');
         }
     });
+});
+
+document.querySelector('nav h2').addEventListener('click', () => {
+    const menu = document.querySelector('nav ul');
+    if (window.innerWidth <= 800) {
+        menu.style.display = menu.style.display == 'block' ? 'none' : 'block';
+    }
 });
